3df705d2e6575f15469acb0c23df47f995d4bea4
prahadeeshwaran <prahadeeshwaran.mk@gmail.com>
2024-01-09 21:55:53 +0530

    Basic io

--- Basic IO.cpp
+++ Basic IO.cpp
@@ -2,6 +2,11 @@
 // Created by praha on 1/9/24.
 //
 #include <iostream>
+using namespace std;
 int main(){
+cout << "hello world!" << endl;
+cout<<"hello "<<"world!"<<endl;
+cout<<"hello "<<"world!\n";
+cout<<"hello\nout\nthere\n";
 
 }
